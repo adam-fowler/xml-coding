837cda9e1b43ecc6cedb94b28c65b484d16fb487
Adam Fowler <adamfowler71@gmail.com>
2019-07-13 12:38:47 +0100

    XMLContainerCodingMap is not XMLCodable

--- Sources/xml-encoder/XMLDecoder.swift
+++ Sources/xml-encoder/XMLDecoder.swift
@@ -23,7 +23,7 @@ public enum XMLContainerCoding {
 }
 
 /// protocol to return XMLContainerCoding values. To control how the child elements of a Codable class are encoded inherit from this and return coding values for each
-public protocol XMLContainerCodingMap {
+public protocol XMLCodable : Codable {
     static func getXMLContainerCoding(for key: CodingKey) -> XMLContainerCoding?
 }
 
@@ -133,7 +133,7 @@ public class XMLDecoder {
 
     /// decode a Codable class from XML
     public func decode<T : Decodable>(_ type: T.Type, from xml: XML.Element) throws -> T {
-        let containerCodingMapType = type as? XMLContainerCodingMap.Type
+        let containerCodingMapType = type as? XMLCodable.Type
         let decoder = _XMLDecoder(xml, options: self.options, containerCodingMapType: containerCodingMapType)
         let value = try T(from: decoder)
         return value
@@ -187,12 +187,12 @@ fileprivate class _XMLDecoder : Decoder {
     var element : XML.Element { return storage.topContainer! }
 
     /// the container coding map for the current element
-    var containerCodingMapType : XMLContainerCodingMap.Type?
+    var containerCodingMapType : XMLCodable.Type?
     
     /// the container encoding for the current element
     var containerCoding : XMLContainerCoding = .default
 
-    public init(_ element : XML.Element, at codingPath: [CodingKey] = [], options: XMLDecoder._Options, containerCodingMapType: XMLContainerCodingMap.Type?) {
+    public init(_ element : XML.Element, at codingPath: [CodingKey] = [], options: XMLDecoder._Options, containerCodingMapType: XMLCodable.Type?) {
         self.storage = _XMLDecoderStorage()
         self.storage.push(container: element)
         self.codingPath = codingPath
@@ -844,7 +844,7 @@ fileprivate class _XMLDecoder : Decoder {
         let prevContainerCodingOwner = self.containerCodingMapType
         defer { self.containerCodingMapType = prevContainerCodingOwner }
         // set the current container coding map
-        containerCodingMapType = type as? XMLContainerCodingMap.Type
+        containerCodingMapType = type as? XMLCodable.Type
         
         if type == Date.self || type == NSDate.self {
             return try self.unbox(element, as: Date.self)
@@ -908,25 +908,38 @@ fileprivate var _iso8601Formatter: ISO8601DateFormatter = {
 // Error Utilities
 //===----------------------------------------------------------------------===//
 
-extension EncodingError {
-    /// Returns a `.invalidValue` error describing the given invalid floating-point value.
+internal extension DecodingError {
+    /// Returns a `.typeMismatch` error describing the expected type.
     ///
+    /// - parameter path: The path of `CodingKey`s taken to decode a value of this type.
+    /// - parameter expectation: The type expected to be encountered.
+    /// - parameter reality: The value that was encountered instead of the expected type.
+    /// - returns: A `DecodingError` with the appropriate path and debug description.
+    static func _typeMismatch(at path: [CodingKey], expectation: Any.Type, reality: Any) -> DecodingError {
+        let description = "Expected to decode \(expectation) but found \(_typeDescription(of: reality)) instead."
+        return .typeMismatch(expectation, Context(codingPath: path, debugDescription: description))
+    }
+    
+    
+    /// Returns a description of the type of `value` appropriate for an error message.
     ///
-    /// - parameter value: The value that was invalid to encode.
-    /// - parameter path: The path of `CodingKey`s taken to encode this value.
-    /// - returns: An `EncodingError` with the appropriate path and debug description.
-    fileprivate static func _invalidFloatingPointValue<T : FloatingPoint>(_ value: T, at codingPath: [CodingKey]) -> EncodingError {
-        let valueDescription: String
-        if value == T.infinity {
-            valueDescription = "\(T.self).infinity"
-        } else if value == -T.infinity {
-            valueDescription = "-\(T.self).infinity"
+    /// - parameter value: The value whose type to describe.
+    /// - returns: A string describing `value`.
+    /// - precondition: `value` is one of the types below.
+    fileprivate static func _typeDescription(of value: Any) -> String {
+        if value is NSNull {
+            return "a null value"
+        } else if value is NSNumber /* FIXME: If swift-corelibs-foundation isn't updated to use NSNumber, this check will be necessary: || value is Int || value is Double */ {
+            return "a number"
+        } else if value is String {
+            return "a string/data"
+        } else if value is [Any] {
+            return "an array"
+        } else if value is [String : Any] {
+            return "a dictionary"
         } else {
-            valueDescription = "\(T.self).nan"
+            return "\(type(of: value))"
         }
-        
-        let debugDescription = "Unable to encode \(valueDescription) directly. Use DictionaryEncoder.NonConformingFloatEncodingStrategy.convertToString to specify how the value should be encoded."
-        return .invalidValue(value, EncodingError.Context(codingPath: codingPath, debugDescription: debugDescription))
     }
 }
 
