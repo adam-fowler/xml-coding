3a67bfd8119ae8488af2a47a9c4b273f11767be5
Adam Fowler <adamfowler71@gmail.com>
2019-07-14 09:22:15 +0100

    Moved docs inside docs folder

--- README.md
+++
@@ -1,3 +0,0 @@
-# xml-encoder
-
-A description of this package.
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import xml_encoderTests
-
-var tests = [XCTestCaseEntry]()
-tests += xml_encoderTests.allTests()
-XCTMain(tests)
