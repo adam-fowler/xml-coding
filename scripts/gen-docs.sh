1277ac2ed56af1ca15c4ff8c4223182a7577661a
Adam Fowler <adamfowler71@gmail.com>
2019-07-19 16:59:13 +0100

    gen-docs: stash everything but docs before checkout gh-pages

--- scripts/gen-docs.sh
+++ scripts/gen-docs.sh
@@ -4,17 +4,26 @@ set -eux
 
 swift package generate-xcodeproj
 jazzy \
+    --clean \
     --author "Adam Fowler" \
     --author_url https://github.com/adam-fowler \
-    --github_url https://github.com/adam-fowler/xml-encoder
+    --github_url https://github.com/adam-fowler/xml-encoder \
+    --copyright "© 2019 Adam Fowler" \
+    --xcodebuild-arguments -project,XMLEncoder.xcodeproj,-scheme,XMLEncoder-Package
+
+# stash everything that isn't in docs
+git stash push -- ":(exclude)docs"
 
 git checkout gh-pages
+# copy contents of docs to docs/current replacing the ones that are already there
 rm -rf docs/current
 mv docs/ current/
 mkdir docs
 mv current/ docs/
+# commit
 git add --all docs
 git commit -m "Publish latest docs"
 git push
+# return to master branch
 git checkout master
-
+git stash pop
